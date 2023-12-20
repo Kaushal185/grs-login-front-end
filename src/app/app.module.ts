c909a898a2f957aa9fc0d8b3c88c377beaf34eab
Kaushal Kawade <kaushal.kawade@oracle.com>
2023-12-20 11:06:02 +0530

    mat table is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from './auth.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,6 +24,13 @@ import { AuthService } from './auth.service';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+    MatFormFieldModule
+    
   ],
   providers: [
     AuthService
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component,OnInit } from '@angular/core';
 import { FieldsService } from '../fields.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ViewChild } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,16 +11,34 @@ import { FieldsService } from '../fields.service';
 })
 export class HomeComponent implements OnInit  {
   constructor(private fieldsService:FieldsService){}
+  displayedColumns: string[] = ['id', 'messageType', 'status', 'createdOn', 'updatedOn', 'message'];
+  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   accounts: any = [];
   ngOnInit(): void {
     this.fieldsService.fetchAccounts().subscribe(
       (data: any[]) => {
         this.accounts = data;
+        this.dataSource.data = data;
         console.log("namaste india");
       },
       (error) => {
         console.error('Error fetching data:', error);
       }
     );
+
+  }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string): void {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
